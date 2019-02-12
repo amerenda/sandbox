f75032e557d21e5df41b6e876afbd53659a080b2
Alex Merenda <amerenda89@gmail.com>
2019-02-12 15:08:26 -0500

    adds tooling (#1)
    * adds python gitignore
    
    * adds centos7 vagrant

--- docker/nginx/start.sh
+++ docker/nginx/start.sh
@@ -2,10 +2,12 @@
 #runs docker, mounts cert dir
 # pull certs down before running
 
+CERT_DIR=
+
 docker run -d \
 --name ssl_test \
--v ~/sandbox/docker/nginx/certs/wildcard-fqtag-cert-5-1-2019:/etc/ssl/certs/ \
+-v ~/sandbox/docker/nginx/certs/${CERT_DIR}:/etc/ssl/certs/ \
 -v ~/sandbox/docker/nginx/config:/etc/nginx/conf.d/ \
--v ~/sandbox/docker/nginx/site/index.hrml:/var/www/site/index.html \
+-v ~/sandbox/docker/nginx/site/index.html:/var/www/site/index.html \
 -p 443:443 \
 nginx:latest
